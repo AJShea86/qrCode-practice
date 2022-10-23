4b56defeecf893b08eb7c77a07b4695c3dc6434e
AJS <andrewsheamls@gmail.com>
2022-10-23 12:12:44 -0600

    qr code generator done

--- qrCode.js
+++ qrCode.js
@@ -1,1 +1,21 @@
-var QRCode = require('qrcode')
+
+
+//  This generates a QR Code and saves in C:\Users\AJ Shea\qrCode (current folder)
+
+//  Created audio file using "Voice Memo" on iphone, then saved into Google Drive
+
+//  Add audio file to Line 15 between " "
+
+//  Run with "npm start"
+
+
+var QRCode = require("qrcode");
+
+QRCode.toFile(
+  "testQrCode.png",          //     This is the name of the generated .png file
+  " ",                       //     Google drive audio file link goes here
+  function (err) {
+    if (err) throw err;
+    console.log("done");    //      Success
+  }
+);
